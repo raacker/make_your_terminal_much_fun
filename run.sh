a5bc79c14f1e3dbbbbd263843da2b0236cb71148
raacker <raacker@naver.com>
2017-04-07 10:52:47 +0900

    edited path script

--- run.sh
+++ run.sh
@@ -2,13 +2,17 @@
 
 apt-get install -y golang
 
-export GOPATH=$HOME/gocode
+echo "export GOPATH=$HOME/gocode" >> ~/.bash_profile
 
 mkdir ~/gocode
 
 go get -u github.com/schachmat/wego
 
-export WEGOPATH=$HOME/gocode/bin/
-export PATH=$PATH:$GOPATH:$WEGOPATH
+echo "export WEGOPATH=$HOME/gocode/bin/" >> ~/.bash_profile
+echo "export PATH=$PATH:$HOME/gocode/:$HOME/gocode/bin/" >> ~/.bash_profile
 
-echo "go get api key from https://darksky.net"
+echo "--------------------------------------------------------------"
+echo "You are all ready. Now go get api key from https://darksky.net"
+echo "And edit ~/.wegorc with your own api"
+echo "Enjoy!"
+echo "--------------------------------------------------------------"
